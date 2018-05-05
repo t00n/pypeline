6a52572d0d7a43a3c3498c2a508a44991cc502d7
t00n <kelkununtel@hotmail.com>
2018-05-05 12:50:51 +0200

    [add] fixture timed-based data with holes

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import random
 
 import pytest
 
@@ -15,4 +16,22 @@ def data_timed():
     for i in range(15):
         d = start + timedelta(seconds=i)
         data.append({'time': d, 'value': i})
+    return data
+
+
+@pytest.fixture
+def data_timed_holes():
+    random.seed(42)
+    data = []
+    start = datetime(2018, 5, 4, 15, 45)
+    i = 0
+    while True:
+        if i > 30:
+            break
+        d = start + timedelta(seconds=i)
+        data.append({'time': d, 'value': i})
+        if random.random() > 0.5:
+            i += random.randint(1, 5)
+        else:
+            i += 1
     return data
